b57e5134777ce8376160e91efb54d19b013a9b4a
eudes-morais <eudes.morais@gmail.com>
2023-11-01 17:51:23 -0300

    Working with OpenAI

--- openai_api/client.py
+++ openai_api/client.py
@@ -6,7 +6,7 @@ def get_car_ai_desc(model, brand_id, year):
         Me mostre uma descrição de venda para o carro {} {} {} em apenas 200 caracteres.
         Fale coisas específicas deste modelo.
         '''
-    openai.api_key = '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'
+    openai.api_key = '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'
     prompt = prompt.format(brand_id, model, year)
     response = openai.Completion.create(
         model='text-davinci-003',
