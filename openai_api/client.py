e26e26a16e6c0c42688176d513653cff2653eff8
eudes-morais <eudes.morais@gmail.com>
2023-11-01 18:19:43 -0300

    Updating client.py with OpenAI

--- openai_api/client.py
+++ openai_api/client.py
@@ -2,12 +2,13 @@ import openai
 
 def get_car_ai_desc(model, brand_id, year):
     # openai.organization = "org-jA7nO9WSCzH8iLdtGvYpyQQG"
-    prompt = ''''
-        Me mostre uma descrição de venda para o carro {} {} {} em apenas 200 caracteres.
-        Fale coisas específicas deste modelo.
-        '''
-    openai.api_key = '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'
-    prompt = prompt.format(brand_id, model, year)
+    # prompt = ''''
+    #     Me mostre uma descrição de venda para o carro {} {} {} em apenas 200 caracteres.
+    #     Fale coisas específicas deste modelo.
+    #     '''
+    # prompt = prompt.format(brand_id, model, year)
+    openai.api_key = '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'
+    prompt = f'Me mostre uma descrição de venda para o carro {brand_id} {model} {year} em apenas 200 caracteres. Fale coisas específicas deste modelo.'
     response = openai.Completion.create(
         model='text-davinci-003',
         prompt=prompt,
